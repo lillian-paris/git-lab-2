17629c9af1067d703a0c1b9815b459eca290fd69
lillian-paris <lp351123@ohio.edu>
2026-01-20 11:58:10 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Lillian Paris
+ *        Date: 1/20/2026
+ * Description: Program will issue text when executed.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
